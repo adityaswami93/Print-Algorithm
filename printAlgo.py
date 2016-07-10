173adefa2d5b94c0f9271dc3556ead9181bee178
Aditya Swami <adityaswami93@gmail.com>
2016-07-11 00:25:03 +0800

    solved the infinity loop problem

--- printAlgo.py
+++ printAlgo.py
@@ -10,6 +10,7 @@ A4_width = 210
 
 A3_width = 297
 A3_height = 420
+tab_identifier = '&' * 8
 
 def print_algo(infilename,outfilename, page_size):
 	i = 0
@@ -40,15 +41,25 @@ def print_algo(infilename,outfilename, page_size):
 			#print line[0]
 			#for letter in line:
 				#if letter == ' ' and !starting_space_removed:
-			line_len = len(line)
+			#print line
+			line = line.replace("	",'&'*8)
+			#print 'this'
+			#print line
+			line_len =  len(line)
 			total_char_len = line_len * char_width
-
+			#print total_char_len
 			if total_char_len > max_width:
 				lines_list = create_print_line(line,max_width)
 				for lines in lines_list:
+					#print lines
+					#print '1111111111'
+					#lines = lines.replace(tab_identifier,'\t')
+					#print lines
+					lines = lines.strip()
 					outfile.write(lines)
 					outfile.write('\n')	
 			else:
+				line = line.replace('&'*8,"	")
 				outfile.write(line)
 				if line_len > 0:
 					outfile.write('\n')
@@ -66,17 +77,27 @@ def create_print_line(line,max_width):
 	line_len = len(line)
 	start = 0
 	ctr = max_width/char_width - 1
-
+	#print 'this'
+	#print line
 	while ctr<line_len:
-		if line[ctr] != ' ':
-			ctr -= 1
-		else:
-			tmp_line = line[start:ctr]
-			print tmp_line
-			tmp_line.split()
+		print ctr
+		if line[ctr].isspace() == True or line[ctr] == '&':
+			tmp_line = line[start:ctr+1]
+			tmp_line = tmp_line.replace('&'*8,"	")
+			tmp_line = tmp_line.strip()
+			#print 'yes1'
+			#print tmp_line
+			#tmp_line.split()
 			start = ctr + 1
 			line_to_print.append(tmp_line)
 			ctr += max_width/char_width
+
+		#elif line[ctr] == '&':
+			#tmp_line = line[start:ctr-7]
+			#tmp_line = tmp_line.replace('&'*8,"	")
+			#tmp_line = tmp_line.strip()
+		else:
+			ctr -= 1
 	else:
 		tmp_line = line[start:]
 		line_to_print.append(tmp_line)
@@ -84,6 +105,14 @@ def create_print_line(line,max_width):
 	return line_to_print
 
 
+def calculate_len(line):
+	total_len = 0
+	for letter in line:
+		if letter == '\t':
+			total_len += 8
+		else:
+			total_len += 1
+	return total_len
 
 
 def usage():
@@ -108,14 +137,10 @@ if __name__ == '__main__':
 		else:
 			assert False, "unhandled option"
 
-		print "1yes"
-
 
 	if inputfilename is None or outputfilename is None or pageSize is None:
 		usage()
 		sys.exit(2)
-
-	print "yes"
 
 	print_algo(inputfilename,outputfilename,pageSize)
 
